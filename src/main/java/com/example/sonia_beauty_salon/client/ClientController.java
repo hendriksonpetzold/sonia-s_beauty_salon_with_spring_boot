e29a6f5cb893d7532bffa96d22e1a8432d346aab
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-12-09 08:40:57 -0300

    feat: put function

--- src/main/java/com/example/sonia_beauty_salon/client/ClientController.java
+++ src/main/java/com/example/sonia_beauty_salon/client/ClientController.java
@@ -3,6 +3,7 @@ package com.example.sonia_beauty_salon.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -29,6 +30,12 @@ public class ClientController {
     @DeleteMapping(path = "{clientId}")
     public void deleteClientById(@PathVariable("clientId") Long clientId){
         clientService.deleteClientById(clientId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateClient(@PathVariable("studentId") Long studentId, @RequestBody Client client){
+        System.out.println("FUNÇÃO PUT");
+        clientService.updateClient(studentId, client);
     }
 
 }
--- src/main/java/com/example/sonia_beauty_salon/client/ClientService.java
+++ src/main/java/com/example/sonia_beauty_salon/client/ClientService.java
@@ -3,6 +3,7 @@ package com.example.sonia_beauty_salon.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -20,11 +21,76 @@ public class ClientService {
     public void deleteClientById(Long clientId){
         boolean exists = clientRepository.existsById(clientId);
         if(!exists){
-            throw new IllegalStateException("Client nao existe");
+            throw new IllegalStateException("Cliente nao existe");
         }
         clientRepository.deleteById(clientId);
     }
+    @Transactional
+    public void updateClient(Long clientId, Client client){
+        Client clientById = clientRepository.findById(clientId).orElseThrow(() -> new IllegalStateException("nao temos esse cliente"));
+        List<Client> clients = clientRepository.findClientQuery();
+        boolean isBookingEmpty = true;
+        if(client.getNameOfClient() != null && client.getNameOfClient().length() > 0){
+            clientById.setNameOfClient(client.getNameOfClient());
+        }
+        if(client.getNameOfAttendant() != null && client.getNameOfAttendant().length() > 0){
+            clientById.setNameOfAttendant(client.getNameOfAttendant());
+        }
+        if(client.getService() != null && client.getService().length() > 0){
+            clientById.setService(client.getService());
+        }
+        if(client.getPrice() != null){
+            clientById.setPrice(client.getPrice());
+        }
+        if(client.getInitialOfBooking() != null || client.getFinishOfBooking() != null){
+            System.out.println("VERIFICADNO NULLO");
+            if(client.getInitialOfBooking() == null){
+                client.setInitialOfBooking(clientById.getInitialOfBooking());
+            }
+            if(client.getFinishOfBooking() == null){
+                client.setFinishOfBooking(clientById.getFinishOfBooking());
+            }
+            if(client.getFinishOfBooking().isAfter(client.getInitialOfBooking())){
+                System.out.println("ENTREI NA FUNÇÃO DO LOOP");
+                if(clients.size() > 0){
 
+                    for(int index =0; index < clients.size(); index++){
+                        Client newClient =  clients.get(index);
+
+
+                        if(client.getInitialOfBooking().isAfter(newClient.getFinishOfBooking())
+                                || client.getFinishOfBooking().isEqual(clientById.getFinishOfBooking())
+                                || client.getInitialOfBooking().isEqual(clientById.getInitialOfBooking())
+                                || client.getInitialOfBooking().isEqual(newClient.getFinishOfBooking())
+                                || client.getFinishOfBooking().isBefore(newClient.getInitialOfBooking())
+                                ||  client.getFinishOfBooking().isEqual(newClient.getInitialOfBooking())
+                        ){
+                            isBookingEmpty = true;
+                        } else {
+                            isBookingEmpty = false;
+                            break;
+                        }
+                    }
+                }
+
+            } else {
+                throw new IllegalStateException("Horário adicionado de forma incorreta");
+            }
+
+            if(isBookingEmpty == true){
+                System.out.println("PUT COM SUCESSO");
+                clientById.setInitialOfBooking(client.getInitialOfBooking());
+                clientById.setFinishOfBooking(client.getFinishOfBooking());
+            } else{
+                System.out.println("JA TEM ESSE HORARIO");
+                throw new IllegalStateException("esse horário ja esta marcado");
+            }
+
+        }
+
+
+
+    }
     public void addNewClient(Client client) {
 
         List<Client> clients = clientRepository.findClientQuery();
@@ -36,8 +102,11 @@ public class ClientService {
                     Client newClient =  clients.get(index);
 
 
-                    if(client.getInitialOfBooking().isAfter(newClient.getFinishOfBooking()) ||
-                            client.getFinishOfBooking().isBefore(newClient.getInitialOfBooking())){
+                    if(client.getInitialOfBooking().isAfter(newClient.getFinishOfBooking())
+                            || client.getInitialOfBooking().isEqual(newClient.getFinishOfBooking())
+                            || client.getFinishOfBooking().isBefore(newClient.getInitialOfBooking())
+                            || client.getFinishOfBooking().isEqual(newClient.getInitialOfBooking())
+                    ){
                         isBookingEmpty = true;
                     } else {
                         isBookingEmpty = false;
